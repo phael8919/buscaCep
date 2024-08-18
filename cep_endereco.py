b6f4de6595d096393f8b41a4b92f1727868472a6
Raphael Vieira <103504959+phael8919@users.noreply.github.com>
2024-08-18 11:40:13 -0300

    Interface Gráfica

--- cep_endereco.py
+++ cep_endereco.py
@@ -1,11 +1,12 @@
 from tkinter import * #Importa a GUI
-from tkinter import messagebox
+from tkinter import messagebox #Exibe uma mensagem de erro caso o cep não seja localizado
 import requests #Captura a URL
 import json #Exibe o conteúdo do json
 
 tela = Tk()
 tela.title('Busca de Endereço pelo CEP')
 
+#Função para capturar o endereço via API
 def buscaCep():
     try:
         cep = entrada.get()
@@ -21,29 +22,33 @@ def buscaCep():
         rotulo3['text'] = f'    Cidade: {dic["localidade"]}'
         rotulo4['text'] = f'UF: {dic["uf"]}'
     except:
-        messagebox.showerror(title='Mensagem de erro', message='Verifique o CEP digitado')
+        messagebox.showerror(title='Mensagem de erro', message='Verifique o CEP digitado!')
         
 
-rotulo = Label(tela, text='    Digite o CEP: ', font='Arial 15')
-rotulo.grid(row=0, column=1, sticky = 'W', pady = 2, padx=2)
+rotulo = Label(tela, text='Digite o CEP: ', font='Arial 15', width=45)
+rotulo.grid(row=0, column=1, sticky = 'W', pady = 20, padx=10)
 
-entrada = Entry(tela, font='Arial 15')
-entrada.grid(row=0, column=2, sticky = W, pady = 2, padx=2)
+#Input para receber o nº do CEP
+entrada = Entry(tela, font='Arial 15', width=45)
+entrada.grid(row=0, column=2, sticky = W, pady = 20, padx=10)
 
+#Botão para executar a função que busca o CEP
 botao = Button(tela, text='Clique aqui', font='Arial 15', command=buscaCep)
-botao.grid(row=0, column=3, sticky = 'W', pady = 2, padx=2)
+botao.grid(row=1,column=1, pady=10, padx=10, sticky='NSEW', columnspan=2)
 
+#Grupo de rótulos que exibem o endereço do CEP buscado
 rotulo1 = Label(tela, text='', font='Arial 15')
-rotulo1.grid(row=1, column=1, sticky = 'W', pady = 4, padx=2)
+rotulo1.grid(row=2, column=1, sticky = 'W', pady = 8)
 
 rotulo2 = Label(tela, text='', font='Arial 15')
-rotulo2.grid(row=1, column=2, sticky = 'W', pady = 4, padx=2)
+rotulo2.grid(row=2, column=2, sticky = 'W', pady = 8)
 
 rotulo3 = Label(tela, text='', font='Arial 15')
-rotulo3.grid(row=2, column=1, sticky = 'W', pady = 4, padx=2)
+rotulo3.grid(row=3, column=1, sticky = 'W', pady = 8)
 
 rotulo4 = Label(tela, text='', font='Arial 15')
-rotulo4.grid(row=2, column=2, sticky = 'W', pady = 4, padx=2)
+rotulo4.grid(row=3, column=2, sticky = 'W', pady = 8)
 
-tela.geometry('650x150')
+#Tamanho da tela
+tela.geometry('1045x250')
 tela.mainloop()
